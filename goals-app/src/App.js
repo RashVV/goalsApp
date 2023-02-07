04139067f1255aa6e6dcadcd31daa314554e16e7
RashVV <vadim.ratushnyak@gmail.com>
2023-02-07 15:21:14 +0200

    46:03 firebase add

--- goals-app/src/App.js
+++ goals-app/src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import { Alert } from './components/Alert';
 import { Navbar } from './components/Navbar';
 import { AlertState } from './context/alert/AlertState';
+import { FirebaseState } from './context/firebase/FirebaseState';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
+    <FirebaseState>
     <AlertState>
     <BrowserRouter>
     <Navbar/>
@@ -20,6 +22,7 @@ function App() {
       </div>
     </BrowserRouter>
     </AlertState>
+    </FirebaseState>
   );
 }
 
